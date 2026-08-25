7718a0118ae9ead38c84052c1e32aa653549b005
MuZaKa66 <freelancemzk@gmail.com>
2026-08-25 20:51:39 +0500

    Add python-vxi11-server as regular files, not nested repo

---
+++ python-vxi11-server/demo_clients/time_client.py
@@ -0,0 +1,23 @@
+import faulthandler
+
+import vxi11_server as vxi11
+
+faulthandler.enable()
+
+# with a default instrument, inst0 is implied.
+default_instr = vxi11.Instrument("TCPIP::127.0.0.1::INSTR")
+time_instr = vxi11.Instrument("TCPIP::127.0.0.1::inst1::INSTR")
+#time_instr = vxi11.Instrument("TCPIP::192.168.2.101::TIME::INSTR")
+
+print('The INSTR instrument:')
+
+default_instr.write('*IDN?')
+print(default_instr.read())
+
+print('contains the following devices:')
+default_instr.write('*DEVICE_LIST?')
+print(default_instr.read())
+
+print()
+print('The TIME device has a current value of:')
+print(time_instr.read())
